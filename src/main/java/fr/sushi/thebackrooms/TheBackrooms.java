e255bedf9838eb12066d50a1da534b481e5c7a13
SushiCannibale <sushi.cannibale@gmail.com>
2025-06-29 00:53:52 +0200

    fix: i f*cking misspelled worldgen. I hate my life ;-;

--- src/main/java/fr/sushi/thebackrooms/TheBackrooms.java
+++ src/main/java/fr/sushi/thebackrooms/TheBackrooms.java
@@ -1,12 +1,11 @@
 package fr.sushi.thebackrooms;
 
-import fr.sushi.thebackrooms.datagen.ModDataGenerators;
-import fr.sushi.thebackrooms.registry.*;
+import fr.sushi.thebackrooms.registry.BlockRegistry;
+import fr.sushi.thebackrooms.registry.CreativeTabRegistry;
+import fr.sushi.thebackrooms.registry.ItemRegistry;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.neoforged.neoforge.common.NeoForge;
 
 @Mod(TheBackrooms.MODID)
 public class TheBackrooms {
@@ -16,8 +15,5 @@ public class TheBackrooms {
         BlockRegistry.register(modEventBus);
         ItemRegistry.register(modEventBus);
         CreativeTabRegistry.register(modEventBus);
-        BiomeFeatureRegistry.register(modEventBus);
-
-        modEventBus.register(ModDataGenerators.class);
     }
 }
